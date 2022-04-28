15ad101571cf4df554155009650733211c688c52
Zhenya <kimzh118@gmail.com>
2022-04-28 16:16:30 +0300

    db-user

---
+++ server/db/migrations/20220428131309-create-user.js
@@ -0,0 +1,37 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      user_name: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      user_email: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+        unique: true,
+      },
+      user_password: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  },
+};
