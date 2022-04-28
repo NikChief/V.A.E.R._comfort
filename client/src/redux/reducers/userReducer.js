c06cbe700fa223de5a4087d20256b63da8804a05
NikChief <niki3000-90@mail.ru>
2022-04-28 20:57:49 +0500

    registration edited

--- client/src/redux/reducers/userReducer.js
+++ client/src/redux/reducers/userReducer.js
@@ -1,8 +1,15 @@
-// import {  } from '../actionTypes/studentsA'
+import { LOGGEDIN_USER } from '../actionTypes/userAT'
+
 const initialState = { user: {} }
+
 
 export function userReducer(state = initialState, action) {
   switch (action.type) {
+    case LOGGEDIN_USER: 
+    return {
+      ...state, user: action.payload
+    }
+
     default:
       return state
   }
