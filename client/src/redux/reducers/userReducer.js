b438d7d2fea855d7a755e5d5f1d5d820de6523ad
NikChief <niki3000-90@mail.ru>
2022-04-29 13:14:40 +0500

    login-logout client 1

--- client/src/redux/reducers/userReducer.js
+++ client/src/redux/reducers/userReducer.js
@@ -1,14 +1,19 @@
-import { LOGGEDIN_USER } from '../actionTypes/userAT'
+import { LOGGEDIN_USER, LOGGEDOUT_USER } from '../actionTypes/userAT'
 
 const initialState = { user: {} }
 
 
 export function userReducer(state = initialState, action) {
   switch (action.type) {
-    case LOGGEDIN_USER: 
-    return {
-      ...state, user: action.payload
-    }
+    case LOGGEDIN_USER:
+      return {
+        ...state, user: action.payload
+      }
+
+    case LOGGEDOUT_USER:
+      return {
+        ...state, user: {}
+      }
 
     default:
       return state
