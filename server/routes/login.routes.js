d6ee8d4c4c974459bf8ac2ae422eaef3949554af
Zhenya <kimzh118@gmail.com>
2022-04-29 14:13:46 +0300

    server-login-route

--- server/routes/login.routes.js
+++ server/routes/login.routes.js
@@ -1,29 +1,47 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable camelcase */
 const router = require('express').Router();
 const bcrypt = require('bcrypt');
-// const { User } = require('../db/models');
+const { User } = require('../db/models');
 
-// router
-//   .route('/')
+router
+  .route('/')
   // .get((req, res) => {
   //   res.render('login');
   // })
-  // .post(async (req, res) => {
-  //   const {
-  //     email, password,
-  //   } = req.body;
+  .post(async (req, res) => {
+    try {
+      const {
+        user_email, user_password,
+      } = req.body;
 
-  //   const user = await User.findOne({
-  //     where: {
-  //       email,
-  //     },
-  //   });
+      const user = await User.findOne({
+        where: {
+          user_email,
+        },
+      });
 
-  //   if (user && await bcrypt.compare(password, user.password)) {
-  //     req.session.user = user;
-  //     res.redirect('/');
-  //   } else {
-  //     res.send('Неправильный адрес электронной почты или пароль.');
-  //   }
-  // });
+      if (user && await bcrypt.compare(user_password, user.password)) {
+        req.session.user = user;
+        res
+          .status(200)
+          .json({
+            message: 'Вход на сайт успешен.',
+          });
+      } else {
+        res
+          .status(400)
+          .json({
+            message: 'Неправильный адрес электронной почты или пароль.',
+          });
+      }
+    } catch (error) {
+      res
+        .status(400)
+        .json({
+          message: `Ошибка регистрации, \n ${error.message}`,
+        });
+    }
+  });
 
 module.exports = router;
