6c31760360841e3e75f4d6cbb75e07b1b1e4d84d
Zhenya <kimzh118@gmail.com>
2022-05-02 10:59:02 +0300

    server-client-logout

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { loggedOutUserAC } from '../../redux/actionCreators/userAC';
-import { LOGOUT_USER } from '../../redux/actionTypes/userAT';
 
 function Nav(props) {
 
   const { user } =useSelector(state => state.userState);
-
-  const navigate = useNavigate();
   const dispatch = useDispatch();
 
   const logout = (e) => {
@@ -27,10 +23,17 @@ function Nav(props) {
     <div>
       <Link to="/" className="logotype">Logo</Link>
       <ul id="" className="login-registration_container">
+      {user ?
+       (<>
+        <li onClick={logout} className=''><h3>Выйти</h3></li>
+       </>
+       )
+      :
+      (<>
         <li ><Link to="/login" className=''><h3>Войти</h3></Link></li>
         <li ><Link to="/registration" className=''><h3>Зарегистрироваться</h3></Link></li>
-        <li onClick={logout} className=''><h3>Выйти</h3></li>
-        {}
+      </>
+      )}
       </ul>
     </div>
   );
