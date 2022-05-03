3c4b39cb4b84250b376260ff921cec4333fa52de
Andrey <surgutandrey96@gmail.com>
2022-05-03 19:58:00 +0300

    saga isUserAuthorized

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -1,5 +1,5 @@
 import { useDispatch } from "react-redux";
-import { loggedInUserAC } from "../../redux/actionCreators/userAC";
+import { fetchIsUserAuthorizedAC } from "../../redux/actionCreators/userAC";
 import { Routes, Route } from "react-router-dom";
 import Nav from '../Nav/Nav';
 import Home from '../Home/Home';
@@ -14,23 +14,15 @@ import BasketList from '../BasketList/BasketList'
 import OrderForm from "../OrderForm/OrderForm";
 
 
-
-
 function App() {
 
   const dispatch = useDispatch();
 
   useEffect(() => {
-    fetch('/login')
-      .then(res => res.json())
-      .then(data => {
-        dispatch(loggedInUserAC(data))
-        // {loggedIn: false, message: 'Пользователь не в системе'}
-        //{loggedIn: true, message: 'Пользователь в системе', userId: 1}
-      })
+    dispatch(fetchIsUserAuthorizedAC())
   }, [dispatch])
 
-  return ( 
+  return (
     <>
       <Nav />
       <TypeList />
--- client/src/redux/sagas/saga.js
+++ client/src/redux/sagas/saga.js
@@ -1,7 +1,7 @@
 import {put, call, takeEvery} from 'redux-saga/effects';
 import { initOrdersAC } from '../actionCreators/ordersAC';
 import { loggedInUserAC, loggedOutUserAC } from '../actionCreators/userAC';
-import { ERR_LOGGEDIN_USER, SAGA_LOGGEDIN_USER, SAGA_LOGOUT_USER, SAGA_REGISTER_USER } from '../actionTypes/userAT';
+import { ERR_LOGGEDIN_USER, SAGA_IS_USER_AUTHORIZED, SAGA_LOGGEDIN_USER, SAGA_LOGOUT_USER, SAGA_REGISTER_USER } from '../actionTypes/userAT';
 import { ERR_ORDERS, SAGA_INIT_ORDERS } from '../actionTypes/ordersAT';
 
 async function fetchData({url, headers, method, body}) {
@@ -85,9 +85,29 @@ function* fetchLoggedOutUser() {
   }
 }
 
+function* fetchIsUserAuthorized() {
+  try {
+    const data = yield call(
+      fetchData, {
+        url: '/login',
+      }
+    );
+    
+    yield put(loggedInUserAC(data));
+  } catch (e) {
+    yield put(
+      {
+        type: ERR_LOGGEDIN_USER, 
+        message: e.message
+      }
+    );
+  }
+}
+
 export function* sagaWatcher() {
   yield takeEvery(SAGA_LOGGEDIN_USER, fetchLoggedInUser)
   yield takeEvery(SAGA_REGISTER_USER, fetchRegisterInUser)
   yield takeEvery(SAGA_LOGOUT_USER, fetchLoggedOutUser)
   yield takeEvery(SAGA_INIT_ORDERS, fetchOrdersInit)
+  yield takeEvery(SAGA_IS_USER_AUTHORIZED, fetchIsUserAuthorized)
 }
