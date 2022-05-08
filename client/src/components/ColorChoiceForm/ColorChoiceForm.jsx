c6ed5c533095490a0b804cb0fc16a6bb9edc634d
Andrey <surgutandrey96@gmail.com>
2022-05-08 21:18:27 +0300

    fix warnings

--- client/src/components/ColorChoiceForm/ColorChoiceForm.jsx
+++ client/src/components/ColorChoiceForm/ColorChoiceForm.jsx
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import { useEffect } from 'react';
+import React from 'react';
 import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
 import Color from '../Color/Color';
