7431250bc11659051e66f7b0c0d7c52f71a43392
Olga <anugallaguna@gmail.com>
2022-05-11 15:18:27 +0300

    size description

--- client/src/components/Item/Item.jsx
+++ client/src/components/Item/Item.jsx
@@ -199,13 +199,13 @@ function Item(props) {
             }
             <div className={styles.sizeInstructionColorDiv}>
             <p className={styles.sizeInstructionColor} data-bs-toggle="modal" data-bs-target="#exampleModal">
-              Инструкция по размерам
+              Инструкция по снятию мерок
             </p>
             <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
               <div className="modal-dialog">
                 <div className="modal-content">
                   <div className="modal-header">
-                    <p className="modal-title" id="exampleModalLabel">Инструкция по размерам</p>
+                    <p className="modal-title" id="exampleModalLabel">Инструкция по снятию мерок</p>
                     <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                   </div>
                   <div className="modal-body">
