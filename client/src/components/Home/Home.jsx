06cf7cb8c4692686ea0767dd8799c3267e51e08d
Andrey <surgutandrey96@gmail.com>
2022-05-11 14:22:19 +0300

    fix carusel

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -4,7 +4,7 @@ import Carousel from '../Carousel/Carousel';
 
 function Home(props) {
   return (
-    <div style={{'display': 'flex', 'width': '100%', 'height': '60vh', 'justifyContent' : 'space-around', 'alignItems': 'center', }}>
+    <div style={{'display': 'flex', 'width': '100%', 'justifyContent' : 'space-around', 'alignItems': 'center', }}>
       <p style={{'width': '400px', 'display' : 'flex', 'alignItems': 'center', 'fontStyle' : 'italic', 'fontSize' : 'large'}}>
       Шьем костюмы и не только по Вашим меркам. С любовью к делу и клиентам. У нас богатая палитра цветов и качественный материал. Наши изделия шьются быстро, а носятся долго и с удовольствием. 
       <br />Отправляем по всей России.
